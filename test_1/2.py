6fffd5e2fc195832e90c23ee2d141566f5b8e221
yehecong <870666103@qq.com>
2018-01-30 18:03:13 +0800

    123
    123

--- test_1/2.py
+++ test_1/2.py
@@ -1,6 +1,6 @@
 
 # coding=utf-8
-
+'''
 print ('ABCDEFG'[:3])
 
 L = range(0, 30);
@@ -17,3 +17,34 @@ print(L);
 
 for i in L:
     print(i);
+
+'''
+
+#用filter求素数
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+
+def primes():
+    yield 2
+    it = _odd_iter() # 初始序列
+    while True:
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) # 构造新序列
+
+
+# 打印1000以内的素数:
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
